10127ee8aaef1d7a3cf5e42e12d411d3d81941f3
Rafael Veloso <rafaelveloso11@gmail.com>
2021-06-25 15:44:42 -0300

    first commit

---
+++ CopiaArquivo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace CopiaArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string origem = @"";
+            string destino = @"";
+            CopiarPasta(origem, destino);
+        }
+
+        private static void CopiarPasta(string dirOrigem,string dirDestino)
+        {
+            try
+            {
+                var arquivos = Directory.GetFiles(dirOrigem);
+                var arqDestino = "";
+                foreach (string arqOrigem in arquivos)
+                {
+                    arqDestino = Path.GetFileName(arqOrigem);
+                    File.Copy(arqOrigem,dirDestino+"\\"+arqDestino,true);
+                    Thread.Sleep(7000);
+                }
+                
+                Console.WriteLine("Cópia encerrada!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("falha: " + ex.Message);
+            }
+        }
+    }
+}
